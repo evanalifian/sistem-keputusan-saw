5cc08f2547446e060f58b8d5b32dcf92e8e55837
evanalifian <evanalifian@gmal.com>
2025-11-26 04:40:53 +0700

    implement and use the data

--- src/Views/landing/hero.php
+++ src/Views/landing/hero.php
@@ -10,12 +10,12 @@
       <div class="hidden sm:mb-8 sm:flex sm:justify-center">
         <div
           class="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-          Semua laporan tersedia pada GitHub. <a href="#" class="font-semibold text-indigo-600"><span class="absolute inset-0"></span>Selengkapnya <span aria-hidden="true">&rarr;</span></a>
+          Semua laporan tersedia pada GitHub. <a href="<?= $data["repository"] ?>" target="_blank" class="font-semibold text-indigo-600"><span class="absolute inset-0"></span>Selengkapnya <span aria-hidden="true">&rarr;</span></a>
         </div>
       </div>
       <div class="text-center">
-        <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Simple Addtive Weighting</h1>
-        <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Sistem Pendukung Keputusan Menentukan Siswa Berprestasi dengan Metode SAW (Simple Addtive Weighting)</p>
+        <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl"><?= $data["title"] ?></h1>
+        <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8"><?= $data["description"] ?></p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
           <a href="#"
             class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Mulai perhitungan</a>
