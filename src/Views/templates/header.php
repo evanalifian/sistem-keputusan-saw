e5ccb43eb24615b3e95ac5f9de0ddb4e8f38b2fe
evanalifian <evanalifian@gmal.com>
2025-11-26 04:41:17 +0700

    update responsive footer and use the data

--- src/Views/templates/header.php
+++ src/Views/templates/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>App</title>
+    <title><?= $data["title"] ?></title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
   </head>
   <body>
