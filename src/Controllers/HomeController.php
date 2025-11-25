1724c2e1072fe52466b474bf65cde847c6308c14
evanalifian <evanalifian@gmal.com>
2025-11-26 04:40:22 +0700

    create property to contain datas

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -5,7 +5,12 @@ namespace App\Saw\Controllers;
 use App\Saw\View;
 
 class HomeController {
+  private static $data = [
+    "title" => "Simple Addtive Weighting",
+    "description" => "Sistem Pendukung Keputusan Menentukan Siswa Berprestasi dengan Metode SAW (Simple Addtive Weighting)",
+    "repository" => "https://github.com/evanalifian/sistem-keputusan-saw"
+  ];
   public function landing() {
-    View::render("landing/index");
+    View::render("landing/index", self::$data);
   }
 }
