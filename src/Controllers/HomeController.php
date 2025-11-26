24d942f12be8752c5ce5e05dc1c2447691b23537
evanalifian <evanalifian@gmal.com>
2025-11-26 23:51:33 +0700

    create new property to store data

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -8,8 +8,30 @@ class HomeController {
   private static $data = [
     "title" => "Simple Addtive Weighting",
     "description" => "Sistem Pendukung Keputusan Menentukan Siswa Berprestasi dengan Metode SAW (Simple Addtive Weighting)",
-    "repository" => "https://github.com/evanalifian/sistem-keputusan-saw"
+    "member" => [
+      [
+        "name" => "FATHAN RUHUL ALAM",
+        "nim" => "240411100190",
+        "photo" => "/member/FATHAN RUHUL ALAM.jpg"
+      ],
+      [
+        "name" => "IMAN WAHYUDY",
+        "nim" => "240411100070",
+        "photo" => "/member/IMAN WAHYUDY.jpg"
+      ],
+      [
+        "name" => "MUHAMMAD AMMAR ABDULHAKIM",
+        "nim" => "240411100109",
+        "photo" => ""
+      ],
+      [
+        "name" => "EVAN RAFA RADYA ALIFIAN",
+        "nim" => "240411100063",
+        "photo" => "/member/EVAN RAFA RADYA ALIFIAN.jpg"
+      ],
+    ]
   ];
+
   public function landing() {
     View::render("landing/index", self::$data);
   }
