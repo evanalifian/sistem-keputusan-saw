e72daa97dd9cc8a869c0e37a707db96d583e070c
evanalifian <evanalifian@gmal.com>
2025-11-26 23:53:40 +0700

    create default data for repository

--- src/View.php
+++ src/View.php
@@ -4,6 +4,7 @@ namespace App\Saw;
 
 class View {
   public static function render($view, $data = []) {
+    $data["repository"] = "https://github.com/evanalifian/sistem-keputusan-saw";
     require_once __DIR__ . "/Views/templates/header.php";
     require_once __DIR__ . "/Views/" . $view . ".php";
     require_once __DIR__ . "/Views/templates/footer.php";
